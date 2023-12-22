cc75cf05e858461d6aa4a675b9c5af278eac35ef
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2023-12-23 00:22:11 +0100

    Advent of Code: setup for years 2015~2022

---
+++ src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode19.java
@@ -0,0 +1,25 @@
+package fr.phoenyx.adventofcode.aoc2015;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdventOfCode19 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode19.class);
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2015/adventofcode19.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null) {
+                // TODO do something
+            }
+            LOGGER.info("PART 1: {}", 0);
+            LOGGER.info("PART 2: {}", 0);
+        }
+    }
+}
---
+++ src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode03.java
@@ -0,0 +1,25 @@
+package fr.phoenyx.adventofcode.aoc2016;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdventOfCode03 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode03.class);
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2016/adventofcode03.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null) {
+                // TODO do something
+            }
+            LOGGER.info("PART 1: {}", 0);
+            LOGGER.info("PART 2: {}", 0);
+        }
+    }
+}
