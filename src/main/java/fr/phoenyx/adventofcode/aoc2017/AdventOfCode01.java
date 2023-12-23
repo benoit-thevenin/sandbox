f145a61deb17eb065ffea2d9a26b8b95548ac0a4
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2023-12-23 22:22:49 +0100

    Advent of Code 2017-01

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode01.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode01.java
@@ -15,11 +15,18 @@ public class AdventOfCode01 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode01.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            int result1 = 0;
+            int result2 = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                int offset = currentLine.length() / 2;
+                for (int i = 0; i < currentLine.length(); i++) {
+                    char c = currentLine.charAt(i);
+                    if (c == currentLine.charAt((i + 1) % currentLine.length())) result1 += c - '0';
+                    if (c == currentLine.charAt((i + offset) % currentLine.length())) result2 += c - '0';
+                }
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", result1);
+            LOGGER.info("PART 2: {}", result2);
         }
     }
 }
