5ca87f4dbe9d5371523bfbf5d603aec9a3934e19
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2023-12-23 22:44:38 +0100

    Advent of Code 2019-01

--- src/main/java/fr/phoenyx/adventofcode/aoc2019/AdventOfCode01.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2019/AdventOfCode01.java
@@ -15,11 +15,26 @@ public class AdventOfCode01 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2019/adventofcode01.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            int result1 = 0;
+            int result2 = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                int moduleMass = Integer.parseInt(currentLine);
+                result1 += moduleMass / 3 - 2;
+                result2 += getFuelRequirements(moduleMass);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", result1);
+            LOGGER.info("PART 2: {}", result2);
         }
+    }
+
+    private static int getFuelRequirements(int mass) {
+        int result = 0;
+        int toAdd = mass / 3 - 2;
+        while (toAdd > 0) {
+            result += toAdd;
+            mass = toAdd;
+            toAdd = mass / 3 - 2;
+        }
+        return result;
     }
 }
