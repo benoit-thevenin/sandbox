8063536556a81604b42a0c4104ec51a6f9aecbee
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2023-12-23 23:53:15 +0100

    Advent of Code 2017-02

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode02.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode02.java
@@ -3,6 +3,9 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +18,22 @@ public class AdventOfCode02 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode02.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            int checksum = 0;
+            int result = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                List<Integer> values = Arrays.stream(currentLine.split("\t")).map(Integer::parseInt).toList();
+                checksum += values.stream().max(Integer::compareTo).orElseThrow() - values.stream().min(Integer::compareTo).orElseThrow();
+                for (int i = 0; i < values.size() - 1; i++) {
+                    for (int j = i + 1; j < values.size(); j++) {
+                        int value1 = values.get(i);
+                        int value2 = values.get(j);
+                        if (value1 % value2 == 0) result += value1 / value2;
+                        if (value2 % value1 == 0) result += value2 / value1;
+                    }
+                }
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", checksum);
+            LOGGER.info("PART 2: {}", result);
         }
     }
 }
