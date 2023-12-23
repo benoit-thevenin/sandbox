177eb0cbbc32a17b8ad325aa0f57a88f491ef0b0
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2023-12-23 23:04:32 +0100

    Advent of Code 2021-01

--- src/main/java/fr/phoenyx/adventofcode/aoc2021/AdventOfCode01.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2021/AdventOfCode01.java
@@ -3,6 +3,8 @@ package fr.phoenyx.adventofcode.aoc2021;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +17,26 @@ public class AdventOfCode01 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2021/adventofcode01.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            List<Integer> values = new ArrayList<>();
+            while ((currentLine = reader.readLine()) != null) values.add(Integer.parseInt(currentLine));
+            LOGGER.info("PART 1: {}", computePart1(values));
+            LOGGER.info("PART 2: {}", computePart2(values));
         }
+    }
+
+    private static int computePart1(List<Integer> values) {
+        int result = 0;
+        for (int i = 1; i < values.size(); i++) if (values.get(i) > values.get(i - 1)) result++;
+        return result;
+    }
+
+    private static int computePart2(List<Integer> values) {
+        int result = 0;
+        for (int i = 1; i < values.size() - 2; i++) {
+            int previousWindow = values.get(i - 1) + values.get(i) + values.get(i + 1);
+            int currentWindow = values.get(i) + values.get(i + 1) + values.get(i + 2);
+            if (currentWindow > previousWindow) result++;
+        }
+        return result;
     }
 }
