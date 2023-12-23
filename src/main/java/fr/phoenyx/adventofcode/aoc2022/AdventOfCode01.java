4ca0bb7add05726fb42fa6626815e0d7b5e0e743
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2023-12-23 23:12:58 +0100

    Advent of Code 2022-01

--- src/main/java/fr/phoenyx/adventofcode/aoc2022/AdventOfCode01.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2022/AdventOfCode01.java
@@ -3,11 +3,18 @@ package fr.phoenyx.adventofcode.aoc2022;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AdventOfCode01 {
+
+    private static class Elf {
+        int calories;
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode01.class);
 
@@ -15,11 +22,18 @@ public class AdventOfCode01 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2022/adventofcode01.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            List<Elf> elves = new ArrayList<>();
+            Elf currentElf = new Elf();
+            elves.add(currentElf);
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (currentLine.isBlank()) {
+                    currentElf = new Elf();
+                    elves.add(currentElf);
+                }
+                else currentElf.calories += Integer.parseInt(currentLine);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", elves.stream().max(Comparator.comparingInt(e -> e.calories)).orElseThrow().calories);
+            LOGGER.info("PART 2: {}", elves.stream().sorted((e1, e2) -> Integer.compare(e2.calories, e1.calories)).limit(3).map(e -> e.calories).reduce(Integer::sum).orElseThrow());
         }
     }
 }
