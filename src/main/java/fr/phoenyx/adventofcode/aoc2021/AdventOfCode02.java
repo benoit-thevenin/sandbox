bda76f83e2cb133bfc21644cca5e4ad8cb4b979d
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2023-12-27 18:14:14 +0100

    Advent of Code 2021-02

--- src/main/java/fr/phoenyx/adventofcode/aoc2021/AdventOfCode02.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2021/AdventOfCode02.java
@@ -14,12 +14,27 @@ public class AdventOfCode02 {
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2021/adventofcode02.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            int horizontalPosition = 0;
+            int depth = 0;
+            int trueDepth = 0;
+            int aim = 0;
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                String[] split = currentLine.split(" ");
+                int value = Integer.parseInt(split[1]);
+                if ("down".equals(split[0])) {
+                    aim += value;
+                    depth += value;
+                } else if ("up".equals(split[0])) {
+                    aim -= value;
+                    depth -= value;
+                } else {
+                    horizontalPosition += value;
+                    trueDepth += aim * value;
+                }
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", horizontalPosition * depth);
+            LOGGER.info("PART 2: {}", horizontalPosition * trueDepth);
         }
     }
 }
