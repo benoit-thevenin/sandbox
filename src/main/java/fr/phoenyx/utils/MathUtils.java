1640c7b7cf07b0fad0e718c4921ae2f4b24c748d
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2023-12-27 11:53:58 +0100

    Advent of Code 2023-24: fixing the throw position calculation

--- src/main/java/fr/phoenyx/utils/MathUtils.java
+++ src/main/java/fr/phoenyx/utils/MathUtils.java
@@ -48,24 +48,6 @@ public class MathUtils {
             return matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1])
             - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[2][0] * matrix[1][2])
             + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[2][0] * matrix[1][1]);
-        if (matrix.length == 4)
-            return matrix[0][0] * (
-                matrix[1][1] * (matrix[2][2] * matrix[3][3] - matrix[2][3] * matrix[3][2])
-              - matrix[1][2] * (matrix[2][1] * matrix[3][3] - matrix[2][3] * matrix[3][1])
-              + matrix[1][3] * (matrix[2][1] * matrix[3][2] - matrix[2][2] * matrix[3][1])
-            ) - matrix[0][1] * (
-                matrix[1][0] * (matrix[2][2] * matrix[3][3] - matrix[2][3] * matrix[3][2])
-              - matrix[1][2] * (matrix[2][0] * matrix[3][3] - matrix[2][3] * matrix[3][0])
-              + matrix[1][3] * (matrix[2][1] * matrix[3][2] - matrix[2][2] * matrix[3][1])
-            ) + matrix[0][2] * (
-                matrix[1][0] * (matrix[2][1] * matrix[3][3] - matrix[2][3] * matrix[3][1])
-              - matrix[1][1] * (matrix[2][0] * matrix[3][3] - matrix[2][3] * matrix[3][0])
-              + matrix[1][3] * (matrix[2][0] * matrix[3][1] - matrix[2][1] * matrix[3][0])
-            ) - matrix[0][3] * (
-                matrix[1][0] * (matrix[2][1] * matrix[3][3] - matrix[2][3] * matrix[3][1])
-              - matrix[1][1] * (matrix[2][0] * matrix[3][3] - matrix[2][3] * matrix[3][0])
-              + matrix[1][2] * (matrix[2][0] * matrix[3][1] - matrix[2][1] * matrix[3][0])
-            );
         double determinant = 0;
         for (int i = 0; i < matrix.length; i++) {
             double ai = i % 2 == 0 ? matrix[0][i] : -matrix[0][i];
