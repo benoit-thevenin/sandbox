52034f07ab2e561b8ccaf2e62191ab4ff32a023b
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2023-12-28 16:35:31 +0100

    Advent of Code 2017-03

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode03.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode03.java
@@ -3,23 +3,69 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.phoenyx.models.Coord;
+import fr.phoenyx.models.Dir;
+
 public class AdventOfCode03 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode03.class);
+    private static final Coord accessPort = new Coord(0, 0);
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode03.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            int input = 0;
+            while ((currentLine = reader.readLine()) != null) input = Integer.parseInt(currentLine);
+            LOGGER.info("PART 1: {}", getCoordinates(input).manhattanDistanceTo(accessPort));
+            LOGGER.info("PART 2: {}", getFirstValueGreaterThan(input));
         }
+    }
+
+    private static Coord getCoordinates(int value) {
+        if (value < 1) throw new IllegalArgumentException("Value must be greater than 0");
+        if (value == 1) return accessPort;
+        int baseX = 0;
+        int baseY = 0;
+        int squareValue = 1;
+        while ((squareValue + 2) * (squareValue + 2) <= value) {
+            squareValue += 2;
+            baseX++;
+            baseY++;
+        }
+        if (squareValue * squareValue == value) return new Coord(baseX, baseY);
+        int inc = 1;
+        int offsetX = 1;
+        int offsetY = 0;
+        while (squareValue * squareValue + inc < value) {
+            if (inc < squareValue + 1) offsetY--;
+            else if (inc < 2 * (squareValue + 1)) offsetX--;
+            else if (inc < 3 * (squareValue + 1)) offsetY++;
+            else offsetX++;
+            inc++;
+        }
+        return new Coord(baseX + offsetX, baseY + offsetY);
+    }
+
+    private static int getFirstValueGreaterThan(int input) {
+        int value = 1;
+        Map<Coord, Integer> coordValues = new HashMap<>();
+        coordValues.put(accessPort, value);
+        while (value <= input) {
+            Coord coord = getCoordinates(coordValues.size() + 1);
+            value = 0;
+            for (Dir dir : Dir.values()) {
+                Coord neighbour = coord.move(dir);
+                if (coordValues.containsKey(neighbour)) value += coordValues.get(neighbour);
+            }
+            coordValues.put(coord, value);
+        }
+        return value;
     }
 }
