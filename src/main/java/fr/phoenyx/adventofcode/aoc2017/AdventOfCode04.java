51a3bacb1d1340feef7ed91db677424f07c6ba37
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-01-09 16:43:38 +0100

    Advent of Code 2017-04

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode04.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode04.java
@@ -3,6 +3,7 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +16,33 @@ public class AdventOfCode04 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode04.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            int count1 = 0;
+            int count2 = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (isValid1(currentLine)) count1++;
+                if (isValid2(currentLine)) count2++;
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", count1);
+            LOGGER.info("PART 2: {}", count2);
         }
+    }
+
+    private static boolean isValid1(String passphrase) {
+        String[] words = passphrase.split(" ");
+        return words.length == Arrays.stream(words).distinct().count();
+    }
+
+    private static boolean isValid2(String passphrase) {
+        String[] words = passphrase.split(" ");
+        for (int i = 0; i < words.length - 1; i++) {
+            char[] sortedChars = words[i].toCharArray();
+            Arrays.sort(sortedChars);
+            for (int j = i + 1; j < words.length; j++) {
+                char[] toCompare = words[j].toCharArray();
+                Arrays.sort(toCompare);
+                if (Arrays.equals(sortedChars, toCompare)) return false;
+            }
+        }
+        return true;
     }
 }
