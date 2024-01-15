c85849fd22cb7903c2a3b3da069d8e3d5230ef8b
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-01-15 17:19:41 +0100

    Advent of Code 2016-05

--- src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode04.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode04.java
@@ -10,6 +10,8 @@ import java.security.NoSuchAlgorithmException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.phoenyx.utils.Utils;
+
 public class AdventOfCode04 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode04.class);
@@ -17,31 +19,22 @@ public class AdventOfCode04 {
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2015/adventofcode04.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            MessageDigest md = MessageDigest.getInstance("MD5");
             String secretKey;
             while ((secretKey = reader.readLine()) != null) {
                 long begin = System.nanoTime();
                 int iteration = 0;
-                String hash = getHash(secretKey + iteration, md);
+                String hash = Utils.getHexadecimalMD5Hash(secretKey + iteration);
                 while (!hash.startsWith("00000")) {
                     iteration ++;
-                    hash = getHash(secretKey + iteration, md);
+                    hash = Utils.getHexadecimalMD5Hash(secretKey + iteration);
                 }
                 LOGGER.info("PART 1: {} ({}), time elpased: {}ms", iteration, hash, (System.nanoTime() - begin) / 1000000);
                 while (!hash.startsWith("000000")) {
                     iteration++;
-                    hash = getHash(secretKey + iteration, md);
+                    hash = Utils.getHexadecimalMD5Hash(secretKey + iteration);
                 }
                 LOGGER.info("PART 2: {} ({}), time elpased: {}ms", iteration, hash, (System.nanoTime() - begin) / 1000000);
             }
         }
-    }
-
-    private static String getHash(String key, MessageDigest md) {
-        md.update(key.getBytes());
-        byte[] digest = md.digest();
-        BigInteger bi = new BigInteger(1, digest);
-        String format = "%0" + (digest.length << 1) + "x";
-        return String.format(format, bi);
     }
 }
--- src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode05.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode05.java
@@ -3,23 +3,63 @@ package fr.phoenyx.adventofcode.aoc2016;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.phoenyx.utils.Utils;
+
 public class AdventOfCode05 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode05.class);
+    private static final Set<Character> positionDigits = Set.of('0', '1', '2', '3', '4', '5', '6', '7');
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2016/adventofcode05.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                String password1 = getPasswordPart1(currentLine);
+                LOGGER.info("PART 1: {}", password1);
+                String password2 = getPasswordPart2(currentLine);
+                LOGGER.info("PART 2: {}", password2);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
         }
+    }
+
+    private static String getPasswordPart1(String key) throws NoSuchAlgorithmException {
+        long begin = System.nanoTime();
+        StringBuilder password = new StringBuilder();
+        int iteration = 0;
+        for (int i = 0; i < 8; i++) {
+            String nextHash = Utils.getHexadecimalMD5Hash(key + iteration);
+            while (!nextHash.startsWith("00000")) {
+                iteration++;
+                nextHash = Utils.getHexadecimalMD5Hash(key + iteration);
+            }
+            LOGGER.info("Found next hash: {}, time elpased: {}ms", nextHash, (System.nanoTime() - begin) / 1000000);
+            password.append(nextHash.charAt(5));
+            iteration++;
+        }
+        return password.toString();
+    }
+
+    private static String getPasswordPart2(String key) throws NoSuchAlgorithmException {
+        long begin = System.nanoTime();
+        char[] password = new char[8];
+        int iteration = 0;
+        for (int i = 0; i < 8; i++) {
+            String nextHash = Utils.getHexadecimalMD5Hash(key + iteration);
+            while (!nextHash.startsWith("00000") || !positionDigits.contains(nextHash.charAt(5)) || password[nextHash.charAt(5) - '0'] != 0) {
+                iteration++;
+                nextHash = Utils.getHexadecimalMD5Hash(key + iteration);
+            }
+            LOGGER.info("Found next hash: {}, time elpased: {}ms", nextHash, (System.nanoTime() - begin) / 1000000);
+            password[nextHash.charAt(5) - '0'] = nextHash.charAt(6);
+            iteration++;
+        }
+        return new String(password);
     }
 }
