8fc0a16cecadb77ff180277a8f27ff6491c946d7
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-01-17 14:09:11 +0100

    Advent of Code 2020-05

--- src/main/java/fr/phoenyx/adventofcode/aoc2020/AdventOfCode05.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2020/AdventOfCode05.java
@@ -3,6 +3,9 @@ package fr.phoenyx.adventofcode.aoc2020;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +17,29 @@ public class AdventOfCode05 {
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2020/adventofcode05.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            List<Integer> ids = new ArrayList<>();
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            while ((currentLine = reader.readLine()) != null) ids.add(getRow(currentLine) * 8 + getCol(currentLine));
+            LOGGER.info("PART 1: {}", ids.stream().max(Integer::compare).orElseThrow());
+            LOGGER.info("PART 2: {}", getMissingId(ids));
         }
+    }
+
+    private static int getRow(String line) {
+        int row = 0;
+        for (int i = 0; i < 7; i++) if (line.charAt(i) == 'B') row += 1 << (6 - i);
+        return row;
+    }
+
+    private static int getCol(String line) {
+        int col = 0;
+        for (int i = 0; i < 3; i++) if (line.charAt(i + 7) == 'R') col += 1 << (2 - i);
+        return col;
+    }
+
+    private static int getMissingId(List<Integer> ids) {
+        Collections.sort(ids);
+        for (int i = 0; i < ids.size() - 1; i++) if (ids.get(i) + 1 != ids.get(i + 1)) return ids.get(i) + 1;
+        return -1;
     }
 }
