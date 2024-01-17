0a1e4c4226c0414004cc589f807a30518f7f091f
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-01-17 17:44:09 +0100

    Advent of Code 2017-06

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode06.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode06.java
@@ -3,11 +3,47 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AdventOfCode06 {
+
+    private static class MemoryBanks {
+        int[] banks;
+
+        MemoryBanks(String line) {
+            String[] split = line.split("\t");
+            banks = new int[split.length];
+            for (int i = 0; i < banks.length; i++) banks[i] = Integer.parseInt(split[i]);
+        }
+
+        MemoryBanks(MemoryBanks memoryBanks) {
+            banks = new int[memoryBanks.banks.length];
+            System.arraycopy(memoryBanks.banks, 0, banks, 0, banks.length);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) return true;
+            if (!(o instanceof MemoryBanks other)) return false;
+            return Arrays.equals(banks, other.banks);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(banks);
+        }
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode06.class);
 
@@ -15,11 +51,36 @@ public class AdventOfCode06 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode06.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            MemoryBanks memoryBanks = new MemoryBanks("0\t2\t7\t0");
+            while ((currentLine = reader.readLine()) != null) memoryBanks = new MemoryBanks(currentLine);
+            Entry<Integer, Integer> result = getCycles(memoryBanks);
+            LOGGER.info("PART 1: {}", result.getKey());
+            LOGGER.info("PART 2: {}", result.getValue());
         }
+    }
+
+    private static Entry<Integer, Integer> getCycles(MemoryBanks memoryBanks) {
+        List<MemoryBanks> states = new ArrayList<>();
+        MemoryBanks current = memoryBanks;
+        int cycles = 0;
+        while (!states.contains(current)) {
+            cycles++;
+            states.add(current);
+            int index = 0;
+            int valueToSpread = current.banks[index];
+            for (int i = 1; i < memoryBanks.banks.length; i++) {
+                if (current.banks[i] > valueToSpread) {
+                    valueToSpread = current.banks[i];
+                    index = i;
+                }
+            }
+            MemoryBanks next = new MemoryBanks(current);
+            next.banks[index] = 0;
+            for (int i = 1; i <= valueToSpread; i++) {
+                next.banks[(index + i) % memoryBanks.banks.length]++;
+            }
+            current = next;
+        }
+        return new SimpleEntry<>(cycles, cycles - states.indexOf(current));
     }
 }
