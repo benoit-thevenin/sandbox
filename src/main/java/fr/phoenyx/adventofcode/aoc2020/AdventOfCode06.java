53d16e2cd9005e2fa0cad1c1b9c2c4bb9c6fcaef
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-01-18 15:13:39 +0100

    Advent of Code 2020-06

--- src/main/java/fr/phoenyx/adventofcode/aoc2020/AdventOfCode06.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2020/AdventOfCode06.java
@@ -3,23 +3,45 @@ package fr.phoenyx.adventofcode.aoc2020;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.phoenyx.utils.Utils;
+
 public class AdventOfCode06 {
+
+    private static class Group {
+        List<String> answers = new ArrayList<>();
+
+        int getAnyoneCount() {
+            return Utils.getLetterCount(String.join("", answers)).size();
+        }
+
+        long getEveryoneCount() {
+            return Utils.getLetterCount(String.join("", answers)).values().stream().filter(v -> v == answers.size()).count();
+        }
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode06.class);
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2020/adventofcode06.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            List<Group> groups = new ArrayList<>();
+            Group current = new Group();
+            groups.add(current);
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (currentLine.isBlank()) {
+                    current = new Group();
+                    groups.add(current);
+                } else current.answers.add(currentLine);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", groups.stream().map(Group::getAnyoneCount).reduce(Integer::sum).orElseThrow());
+            LOGGER.info("PART 2: {}", groups.stream().map(Group::getEveryoneCount).reduce(Long::sum).orElseThrow());
         }
     }
 }
