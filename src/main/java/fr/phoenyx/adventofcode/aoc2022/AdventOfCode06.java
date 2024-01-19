6138682b7509c3aa6e7aebe60617bd9b1e6c2bfb
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-01-19 15:54:21 +0100

    Advent of Code 2022-06

--- src/main/java/fr/phoenyx/adventofcode/aoc2022/AdventOfCode06.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2022/AdventOfCode06.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.phoenyx.utils.Utils;
+
 public class AdventOfCode06 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode06.class);
@@ -14,12 +16,18 @@ public class AdventOfCode06 {
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2022/adventofcode06.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String message = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            while ((currentLine = reader.readLine()) != null) message = currentLine;
+            LOGGER.info("PART 1: {}", getMarkerPosition(message, 4));
+            LOGGER.info("PART 2: {}", getMarkerPosition(message, 14));
         }
+    }
+
+    private static int getMarkerPosition(String message, int length) {
+        for (int i = 0; i < message.length() - length; i++) {
+            if (Utils.getLetterCount(message.substring(i, i + length)).values().stream().allMatch(v -> v == 1)) return i + length;
+        }
+        throw new IllegalArgumentException("Invalid message");
     }
 }
