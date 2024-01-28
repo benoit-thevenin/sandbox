ff8fe5892fc1ab4edb4e15eaf3ec2cdd21379a17
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-01-28 11:36:26 +0100

    Advent of Code 2015-05 : Refactor characters loop analysis to regex

--- src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode05.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode05.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,6 @@ public class AdventOfCode05 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode05.class);
     private static final List<Character> vowels = List.of('a', 'e', 'i', 'o', 'u');
-    private static final List<String> forbidden = List.of("ab", "cd", "pq", "xy");
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2015/adventofcode05.txt";
@@ -31,35 +31,12 @@ public class AdventOfCode05 {
     }
 
     private static boolean isNice1(String s) {
-        for (String f : forbidden) if (s.contains(f)) return false;
-        int vowelCount = 0;
-        boolean hasDoubledLetter = false;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (vowels.contains(c)) vowelCount++;
-            if (i < s.length() - 1 && c == s.charAt(i + 1)) hasDoubledLetter = true;
-        }
-        return vowelCount >= 3 && hasDoubledLetter;
+        return !Pattern.matches(".*((ab)|(cd)|(pq)|(xy)).*", s)
+            && Pattern.matches("(.*[aeiou].*){3,}", s)
+            && Pattern.matches(".*([a-z])\\1.*", s);
     }
 
     private static boolean isNice2(String s) {
-        boolean hasDoubledLetter = false;
-        for (int i = 0; i < s.length() - 2; i++) {
-            if (s.charAt(i) == s.charAt(i + 2)) {
-                hasDoubledLetter = true;
-                break;
-            }
-        }
-        if (!hasDoubledLetter) return false;
-        List<String> letterPairs = new ArrayList<>();
-        for (int i = 0; i < s.length() - 1; i++) {
-            String pair = s.charAt(i) + "" + s.charAt(i + 1);
-            String last = null;
-            if (!letterPairs.isEmpty()) last = letterPairs.remove(letterPairs.size() - 1);
-            if (letterPairs.contains(pair)) return true;
-            if (last != null) letterPairs.add(last);
-            letterPairs.add(pair);
-        }
-        return false;
+        return Pattern.matches(".*([a-z]).\\1.*", s) && Pattern.matches(".*([a-z]{2}).*\\1.*", s);
     }
 }
