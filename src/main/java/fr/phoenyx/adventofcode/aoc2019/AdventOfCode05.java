c67dcc43e5736596fce4b46321f0adf63e0e9fd6
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-02-02 20:40:11 +0100

    Advent of Code 2019-07

--- src/main/java/fr/phoenyx/adventofcode/aoc2019/AdventOfCode05.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2019/AdventOfCode05.java
@@ -3,13 +3,59 @@ package fr.phoenyx.adventofcode.aoc2019;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AdventOfCode05 {
+
+    public static class IntcodeComputer {
+        int[] program;
+        int index = 0;
+
+        IntcodeComputer(int[] program) {
+            this.program = new int[program.length];
+            System.arraycopy(program, 0, this.program, 0, program.length);
+        }
+
+        int run(int parameter) {
+            boolean parameterUsed = false;
+            while (true) {
+                if (program[index] % 100 == 99) return 0;
+                int opCode = program[index] % 10;
+                int first = (program[index] / 100) % 10 == 0 ? program[program[index + 1]] : program[index + 1];
+                int second = 0;
+                if (opCode != 3 && opCode != 4) second = (program[index] / 1000) % 10 == 0 ? program[program[index + 2]] : program[index + 2];
+                if (opCode == 1) {
+                    program[program[index + 3]] = first + second;
+                    index += 4;
+                } else if (opCode == 2) {
+                    program[program[index + 3]] = first * second;
+                    index += 4;
+                } else if (opCode == 3) {
+                    if (parameterUsed) return -2;
+                    parameterUsed = true;
+                    program[program[index + 1]] = parameter;
+                    index += 2;
+                } else if (opCode == 4) {
+                    index += 2;
+                    if (first != 0) return first;
+                } else if (opCode == 5) {
+                    if (first != 0) index = second;
+                    else index += 3;
+                } else if (opCode == 6) {
+                    if (first == 0) index = second;
+                    else index += 3;
+                } else if (opCode == 7) {
+                    program[program[index + 3]] = first < second ? 1 : 0;
+                    index += 4;
+                } else if (opCode == 8) {
+                    program[program[index + 3]] = first == second ? 1 : 0;
+                    index += 4;
+                } else return -1;
+            }
+        }
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode05.class);
 
@@ -23,50 +69,8 @@ public class AdventOfCode05 {
                 program = new int[split.length];
                 for (int i = 0; i < split.length; i++) program[i] = Integer.parseInt(split[i]);
             }
-            LOGGER.info("PART 1: {}", run(program, 1));
-            LOGGER.info("PART 2: {}", run(program, 5));
-        }
-    }
-
-    private static int run(int[] program, int parameter) {
-        List<Integer> outputs = new ArrayList<>();
-        int[] save = new int[program.length];
-        System.arraycopy(program, 0, save, 0, program.length);
-        int index = 0;
-        while (true) {
-            if (program[index] % 100 == 99) {
-                System.arraycopy(save, 0, program, 0, program.length);
-                return outputs.get(outputs.size() - 1);
-            }
-            int opCode = program[index] % 10;
-            int first = (program[index] / 100) % 10 == 0 ? program[program[index + 1]] : program[index + 1];
-            int second = 0;
-            if (opCode != 3 && opCode != 4) second = (program[index] / 1000) % 10 == 0 ? program[program[index + 2]] : program[index + 2];
-            if (opCode == 1) {
-                program[program[index + 3]] = first + second;
-                index += 4;
-            } else if (opCode == 2) {
-                program[program[index + 3]] = first * second;
-                index += 4;
-            } else if (opCode == 3) {
-                program[program[index + 1]] = parameter;
-                index += 2;
-            } else if (opCode == 4) {
-                outputs.add(first);
-                index += 2;
-            } else if (opCode == 5) {
-                if (first != 0) index = second;
-                else index += 3;
-            } else if (opCode == 6) {
-                if (first == 0) index = second;
-                else index += 3;
-            } else if (opCode == 7) {
-                program[program[index + 3]] = first < second ? 1 : 0;
-                index += 4;
-            } else if (opCode == 8) {
-                program[program[index + 3]] = first == second ? 1 : 0;
-                index += 4;
-            } else return -1;
+            LOGGER.info("PART 1: {}", new IntcodeComputer(program).run(1));
+            LOGGER.info("PART 2: {}", new IntcodeComputer(program).run(5));
         }
     }
 }
--- src/main/java/fr/phoenyx/adventofcode/aoc2019/AdventOfCode07.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2019/AdventOfCode07.java
@@ -3,9 +3,13 @@ package fr.phoenyx.adventofcode.aoc2019;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import fr.phoenyx.adventofcode.aoc2019.AdventOfCode05.IntcodeComputer;
+import fr.phoenyx.utils.MathUtils;
 
 public class AdventOfCode07 {
 
@@ -15,11 +19,58 @@ public class AdventOfCode07 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2019/adventofcode07.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            int[] program = new int[0];
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                String[] split = currentLine.split(",");
+                program = new int[split.length];
+                for (int i = 0; i < split.length; i++) program[i] = Integer.parseInt(split[i]);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", getMaximumThrustersOutput(program));
+            LOGGER.info("PART 2: {}", getMaximumThrustersOutputWithFeedbackLoop(program));
         }
+    }
+
+    private static int getMaximumThrustersOutput(int[] program) {
+        IntcodeComputer[] amplifiers = new IntcodeComputer[5];
+        int maxOutput = Integer.MIN_VALUE;
+        List<int[]> permutations = MathUtils.getAllPermutations(new int[]{0, 1, 2, 3, 4});
+        for (int i = 0; i < permutations.size() - 1; i++) {
+            int current = 0;
+            int[] phases = permutations.get(i);
+            for (int j = 0; j < 5; j++) {
+                amplifiers[j] = new IntcodeComputer(program);
+                amplifiers[j].run(phases[j]);
+                current = amplifiers[j].run(current);
+            }
+            if (current > maxOutput) maxOutput = current;
+        }
+        return maxOutput;
+    }
+
+    private static int getMaximumThrustersOutputWithFeedbackLoop(int[] program) {
+        IntcodeComputer[] amplifiers = new IntcodeComputer[5];
+        int maxOutput = Integer.MIN_VALUE;
+        List<int[]> permutations = MathUtils.getAllPermutations(new int[]{5, 6, 7, 8, 9});
+        for (int i = 0; i < permutations.size() - 1; i++) {
+            int[] phases = permutations.get(i);
+            for (int j = 0; j < 5; j++) {
+                amplifiers[j] = new IntcodeComputer(program);
+                amplifiers[j].run(phases[j]);
+            }
+            int current = 0;
+            while (true) {
+                int save = current;
+                current = amplifiers[0].run(current);
+                if (current == 0) {
+                    current = save;
+                    break;
+                }
+                for (int j = 1; j < 5; j++) {
+                    current = amplifiers[j].run(current);
+                }
+            }
+            if (current > maxOutput) maxOutput = current;
+        }
+        return maxOutput;
     }
 }
