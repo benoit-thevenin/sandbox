b343827f705b95ab3a3987e30ffd8af1d63cfce8
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-02-07 16:31:41 +0100

    Advent of Code 2015-08

--- src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode08.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode08.java
@@ -14,12 +14,32 @@ public class AdventOfCode08 {
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2015/adventofcode08.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            int totalInitialCharacters = 0;
+            int totalInMemoryCharacters = 0;
+            int totalEncodedCharacters = 0;
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                totalInitialCharacters += currentLine.length();
+                totalInMemoryCharacters += getInMemoryString(currentLine).length();
+                totalEncodedCharacters += getEncodedString(currentLine).length();
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", totalInitialCharacters - totalInMemoryCharacters);
+            LOGGER.info("PART 2: {}", totalEncodedCharacters - totalInitialCharacters);
         }
+    }
+
+    private static String getInMemoryString(String encodedString) {
+        String inMemoryString = encodedString.substring(1, encodedString.length() - 1);
+        inMemoryString = inMemoryString.replaceAll("\\\\x[0-9a-f]{2}", "?");
+        inMemoryString = inMemoryString.replace("\\\"", "\"");
+        inMemoryString = inMemoryString.replace("\\\\", "\\");
+        return inMemoryString;
+    }
+
+    private static String getEncodedString(String s) {
+        String encodedString = s.replace("\\", "\\\\");
+        encodedString = encodedString.replace("\"", "\\\"");
+        encodedString = "\"" + encodedString + "\"";
+        return encodedString;
     }
 }
