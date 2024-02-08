5755380d08a014dc4d82e5746b77784fbbb0f291
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-02-08 15:14:12 +0100

    Advent of Code 2016-08

--- src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode08.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode08.java
@@ -15,11 +15,62 @@ public class AdventOfCode08 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2016/adventofcode08.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            boolean[][] screen = new boolean[50][6];
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (currentLine.startsWith("rect")) processRectInstruction(currentLine, screen);
+                else if (currentLine.contains("y=")) processRotateRow(currentLine, screen);
+                else processRotateCol(currentLine, screen);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", countLitPixels(screen));
+            LOGGER.info("PART 2: {}", printScreen(screen));
         }
+    }
+
+    private static void processRectInstruction(String instruction, boolean[][] screen) {
+        String[] dimensions = instruction.split("rect ")[1].split("x");
+        int width = Integer.parseInt(dimensions[0]);
+        int height = Integer.parseInt(dimensions[1]);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) screen[i][j] = true;
+        }
+    }
+
+    private static void processRotateRow(String instruction, boolean[][] screen) {
+        String[] values = instruction.split("=")[1].split(" by ");
+        int row = Integer.parseInt(values[0]);
+        int shift = Integer.parseInt(values[1]);
+        boolean[] newRow = new boolean[50];
+        for (int i = 0; i < 50; i++) newRow[(i + shift) % 50] = screen[i][row];
+        for (int i = 0; i < 50; i++) screen[i][row] = newRow[i];
+    }
+
+    private static void processRotateCol(String instruction, boolean[][] screen) {
+        String[] values = instruction.split("=")[1].split(" by ");
+        int col = Integer.parseInt(values[0]);
+        int shift = Integer.parseInt(values[1]);
+        boolean[] newCol = new boolean[6];
+        for (int i = 0; i < 6; i++) newCol[(i + shift) % 6] = screen[col][i];
+        System.arraycopy(newCol, 0, screen[col], 0, 6);
+    }
+
+    private static int countLitPixels(boolean[][] screen) {
+        int litPixels = 0;
+        for (int i = 0; i < 50; i++) {
+            for (int j = 0; j < 6; j++) if (screen[i][j]) litPixels++;
+        }
+        return litPixels;
+    }
+
+    private static String printScreen(boolean[][] screen) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('\n');
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 50; j++) {
+                if (screen[j][i]) sb.append('#');
+                else sb.append('.');
+            }
+            sb.append('\n');
+        }
+        return sb.toString();
     }
 }
