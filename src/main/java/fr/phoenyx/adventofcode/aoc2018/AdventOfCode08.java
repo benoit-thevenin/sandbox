8d3937019fea2d45ed31b019d78a38fd36d13f56
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-02-09 10:41:34 +0100

    Advent of Code 2018-08

--- src/main/java/fr/phoenyx/adventofcode/aoc2018/AdventOfCode08.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2018/AdventOfCode08.java
@@ -3,23 +3,63 @@ package fr.phoenyx.adventofcode.aoc2018;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AdventOfCode08 {
 
+    private static class Node {
+        final Node ancestor;
+        final List<Node> children = new ArrayList<>();
+        final List<Integer> metadata = new ArrayList<>();
+        private int value = -1;
+
+        Node(Node ancestor) {
+            this.ancestor = ancestor;
+        }
+
+        int getValue() {
+            if (value != -1) return value;
+            if (children.isEmpty()) value = metadata.stream().reduce(Integer::sum).orElseThrow();
+            else {
+                value = 0;
+                for (int index : metadata) if (children.size() >= index) value += children.get(index - 1).getValue();
+            }
+            return value;
+        }
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode08.class);
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2018/adventofcode08.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            List<Node> tree = new ArrayList<>();
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                List<Integer> entries = Arrays.stream(currentLine.split(" ")).map(Integer::parseInt).toList();
+                buildTree(entries, 0, null, tree);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            tree.forEach(node -> {
+                if (node.ancestor != null) node.ancestor.children.add(node);
+            });
+            LOGGER.info("PART 1: {}", tree.stream().flatMap(n -> n.metadata.stream()).reduce(Integer::sum).orElseThrow());
+            LOGGER.info("PART 2: {}", tree.get(0).getValue());
         }
+    }
+
+    private static int buildTree(List<Integer> entries, int index, Node ancestor, List<Node> tree) {
+        int childQuantity = entries.get(index);
+        int metadataQuantity = entries.get(index + 1);
+        index += 2;
+        Node node = new Node(ancestor);
+        tree.add(node);
+        for (int i = 0; i < childQuantity; i++) index = buildTree(entries, index, node, tree);
+        for (int i = 0; i < metadataQuantity; i++) node.metadata.add(entries.get(index + i));
+        return index + metadataQuantity;
     }
 }
