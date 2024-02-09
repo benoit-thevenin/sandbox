6aa66c25373b638a027395e0059a9c99a738f465
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-02-09 09:24:16 +0100

    Advent of Code 2017-08

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode08.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode08.java
@@ -3,6 +3,9 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.IntBinaryOperator;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +18,29 @@ public class AdventOfCode08 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode08.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            Map<String, Integer> registers = new HashMap<>();
+            int highestValue = Integer.MIN_VALUE;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                String[] split = currentLine.split(" ");
+                int checkValue = registers.getOrDefault(split[4], 0);
+                int conditionValue = Integer.parseInt(split[6]);
+                if (isConditionMet(checkValue, split[5], conditionValue)) {
+                    IntBinaryOperator operation = "inc".equals(split[1]) ? Integer::sum : (a, b) -> a - b;
+                    registers.put(split[0], operation.applyAsInt(registers.getOrDefault(split[0], 0), Integer.parseInt(split[2])));
+                    if (registers.get(split[0]) > highestValue) highestValue = registers.get(split[0]);
+                }
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", registers.values().stream().max(Integer::compare).orElseThrow());
+            LOGGER.info("PART 2: {}", highestValue);
         }
+    }
+
+    private static boolean isConditionMet(int checkValue, String operator, int conditionValue) {
+        return "<".equals(operator) && checkValue < conditionValue
+            || "<=".equals(operator) && checkValue <= conditionValue
+            || ">".equals(operator) && checkValue > conditionValue
+            || ">=".equals(operator) && checkValue >= conditionValue
+            || "==".equals(operator) && checkValue == conditionValue
+            || "!=".equals(operator) && checkValue != conditionValue;
     }
 }
