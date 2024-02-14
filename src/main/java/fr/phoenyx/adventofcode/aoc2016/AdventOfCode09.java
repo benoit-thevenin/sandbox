75f486fe2ba107868d6c77cb28842f29e87c8756
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-02-14 12:37:22 +0100

    Advent of Code 2016-09

--- src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode09.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode09.java
@@ -16,10 +16,57 @@ public class AdventOfCode09 {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                LOGGER.info("PART 1: {}", getDecompressedLength(currentLine));
+                LOGGER.info("PART 2: {}", getDecompressedLengthV2(currentLine));
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
         }
+    }
+
+    private static int getDecompressedLength(String s) {
+        int length = 0;
+        int index = 0;
+        while (index < s.length()) {
+            if (s.charAt(index) != '(') {
+                length++;
+                index++;
+            } else {
+                String marker = getMarker(s, index + 1);
+                index += marker.length() + 2;
+                String[] split = marker.split("x");
+                int markerLength = Integer.parseInt(split[0]);
+                length += markerLength * Integer.parseInt(split[1]);
+                index += markerLength;
+            }
+        }
+        return length;
+    }
+
+    private static long getDecompressedLengthV2(String s) {
+        long length = 0;
+        int index = 0;
+        while (index < s.length()) {
+            if (s.charAt(index) != '(') {
+                length++;
+                index++;
+            } else {
+                String marker = getMarker(s, index + 1);
+                index += marker.length() + 2;
+                String[] split = marker.split("x");
+                int markerLength = Integer.parseInt(split[0]);
+                int repetitions = Integer.parseInt(split[1]);
+                length += repetitions * getDecompressedLengthV2(s.substring(index, index + markerLength));
+                index += markerLength;
+            }
+        }
+        return length;
+    }
+
+    private static String getMarker(String s, int index) {
+        StringBuilder marker = new StringBuilder();
+        while (s.charAt(index) != ')') {
+            marker.append(s.charAt(index));
+            index++;
+        }
+        return marker.toString();
     }
 }
