648d8972e48481521b96afc3450f7481b14fe846
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-02-14 14:20:43 +0100

    Advent of Code 2017-09

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode09.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode09.java
@@ -3,6 +3,8 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,10 +18,33 @@ public class AdventOfCode09 {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                Entry<Integer, Integer> result = getResult(currentLine);
+                LOGGER.info("PART 1: {}", result.getKey());
+                LOGGER.info("PART 2: {}", result.getValue());
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
         }
+    }
+
+    private static Entry<Integer, Integer> getResult(String s) {
+        int score = 0;
+        int garbageLength = 0;
+        int index = 0;
+        int depth = 0;
+        boolean isInGarbage = false;
+        while (index < s.length()) {
+            if (s.charAt(index) == '!') index++;
+            else if (isInGarbage) {
+                if (s.charAt(index) == '>') isInGarbage = false;
+                else garbageLength++;
+            } else {
+                if (s.charAt(index) == '{') {
+                    depth++;
+                    score += depth;
+                } else if (s.charAt(index) == '}') depth--;
+                else if (s.charAt(index) == '<') isInGarbage = true;
+            }
+            index++;
+        }
+        return new SimpleEntry<>(score, garbageLength);
     }
 }
