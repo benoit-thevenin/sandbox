19c9cb1f12c5d7abcec6ac637b39931f6a427b9e
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-02-28 13:19:59 +0100

    Advent of Code 2015-12

--- src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode12.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode12.java
@@ -3,7 +3,10 @@ package fr.phoenyx.adventofcode.aoc2015;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,10 +19,31 @@ public class AdventOfCode12 {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                JSONObject json = new JSONObject(currentLine);
+                LOGGER.info("PART 1: {}", getSumOfNumbers(json.toMap(), true));
+                LOGGER.info("PART 2: {}", getSumOfNumbers(json.toMap(), false));
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
         }
+    }
+
+    private static int getSumOfNumbers(Map<?, ?> json, boolean isPart1) {
+        if (!isPart1 && json.containsValue("red")) return 0;
+        int sum = 0;
+        for (Object o : json.values()) {
+            if (o instanceof Integer v) sum += v;
+            else if (o instanceof Map<?, ?> subJson) sum += getSumOfNumbers(subJson, isPart1);
+            else if (o instanceof List<?> jsonArray) sum += getSumOfNumbers(jsonArray, isPart1);
+        }
+        return sum;
+    }
+
+    private static int getSumOfNumbers(List<?> jsonArray, boolean isPart1) {
+        int sum = 0;
+        for (Object o : jsonArray) {
+            if (o instanceof Integer v) sum += v;
+            else if (o instanceof Map<?, ?> subJson) sum += getSumOfNumbers(subJson, isPart1);
+            else if (o instanceof List<?> subJsonArray) sum += getSumOfNumbers(subJsonArray, isPart1);
+        }
+        return sum;
     }
 }
