e0ad9aed8f92631c3566c4f48cd638ded09db003
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-03-08 12:03:30 +0100

    Advent of Code 2022-12

--- src/main/java/fr/phoenyx/adventofcode/aoc2022/AdventOfCode12.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2022/AdventOfCode12.java
@@ -3,9 +3,21 @@ package fr.phoenyx.adventofcode.aoc2022;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import fr.phoenyx.models.CharGrid;
+import fr.phoenyx.models.coords.Coord2;
+import fr.phoenyx.models.coords.Dir;
 
 public class AdventOfCode12 {
 
@@ -14,12 +26,56 @@ public class AdventOfCode12 {
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2022/adventofcode12.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            List<String> lines = new ArrayList<>();
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            while ((currentLine = reader.readLine()) != null) lines.add(currentLine);
+            CharGrid grid = new CharGrid(lines);
+            LOGGER.info("PART 1: {}", getShortestPathLength(grid, 'S'));
+            LOGGER.info("PART 2: {}", getShortestPathLength(grid, 'a'));
         }
+    }
+
+    private static int getShortestPathLength(CharGrid grid, char start) {
+        return getStartingPoints(grid, start).stream()
+            .map(s -> getShortestPathLength(grid, s))
+            .filter(l -> l > 0)
+            .min(Integer::compare).orElseThrow();
+    }
+
+    private static Set<Coord2> getStartingPoints(CharGrid grid, char start) {
+        Set<Coord2> startingPoints = new HashSet<>();
+        for (int i = 0; i < grid.width; i++) {
+            for (int j = 0; j < grid.height; j++) if (grid.grid[i][j] == start) startingPoints.add(new Coord2(i, j));
+        }
+        return startingPoints;
+    }
+
+    private static int getShortestPathLength(CharGrid grid, Coord2 start) {
+        Map<Coord2, Integer> visited = new HashMap<>();
+        Queue<Coord2> toVisit = new LinkedList<>();
+        visited.put(start, 0);
+        toVisit.add(start);
+        while (!toVisit.isEmpty()) {
+            Coord2 current = toVisit.remove();
+            for (Dir dir : Dir.FOUR_NEIGHBOURS_VALUES) {
+                Coord2 neighbour = current.move(dir);
+                if (canVisit(grid, visited, current, neighbour)) {
+                    if (grid.grid[neighbour.x][neighbour.y] == 'E') return visited.get(current) + 1;
+                    toVisit.add(neighbour);
+                    visited.put(neighbour, visited.get(current) + 1);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static boolean canVisit(CharGrid grid, Map<Coord2, Integer> visited, Coord2 current, Coord2 neighbour) {
+        return grid.isInGrid(neighbour.x, neighbour.y) && !visited.containsKey(neighbour) && getHeight(grid, neighbour) <= getHeight(grid, current) + 1;
+    }
+
+    private static int getHeight(CharGrid grid, Coord2 coord) {
+        if (grid.grid[coord.x][coord.y] == 'S') return 0;
+        if (grid.grid[coord.x][coord.y] == 'E') return 25;
+        return grid.grid[coord.x][coord.y] - 'a';
     }
 }
