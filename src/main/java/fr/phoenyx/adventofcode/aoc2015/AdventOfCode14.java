dd9ba0efa6d4f4e62781ddd70bb2e5fac812d3f1
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-03-21 17:18:32 +0100

    Advent of Code 2015-14

--- src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode14.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2015/AdventOfCode14.java
@@ -3,11 +3,34 @@ package fr.phoenyx.adventofcode.aoc2015;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AdventOfCode14 {
+
+    private static class Reindeer {
+        final int speed;
+        final int flyTime;
+        final int restTime;
+
+        Reindeer(String line) {
+            String[] split = line.split(" ");
+            speed = Integer.parseInt(split[3]);
+            flyTime = Integer.parseInt(split[6]);
+            restTime = Integer.parseInt(split[13]);
+        }
+
+        int getDistanceAfter(int time) {
+            int cycles = time / (flyTime + restTime);
+            int cyclesModulo = time % (flyTime + restTime);
+            return cycles * speed * flyTime + Math.min(cyclesModulo, flyTime) * speed;
+        }
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode14.class);
 
@@ -15,11 +38,21 @@ public class AdventOfCode14 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2015/adventofcode14.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            List<Reindeer> reindeers = new ArrayList<>();
+            while ((currentLine = reader.readLine()) != null) reindeers.add(new Reindeer(currentLine));
+            LOGGER.info("PART 1: {}", reindeers.stream().map(r -> r.getDistanceAfter(2503)).max(Integer::compare).orElseThrow());
+            LOGGER.info("PART 2: {}", getMaxPointsAfter2503seconds(reindeers));
         }
+    }
+
+    private static int getMaxPointsAfter2503seconds(List<Reindeer> reindeers) {
+        Map<Reindeer, Integer> points = new HashMap<>();
+        reindeers.forEach(r -> points.put(r, 0));
+        for (int i = 1; i <= 2503; i++) {
+            int time = i;
+            int maxDistance = reindeers.stream().map(r -> r.getDistanceAfter(time)).max(Integer::compare).orElseThrow();
+            reindeers.stream().filter(r -> r.getDistanceAfter(time) == maxDistance).forEach(r -> points.put(r, points.get(r) + 1));
+        }
+        return points.values().stream().max(Integer::compare).orElseThrow();
     }
 }
