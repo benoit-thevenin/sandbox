431c8985ea39a51b9bcfd1364f48c44690d5939f
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-04-18 11:35:28 +0200

    Advent of Code 2020-14

--- src/main/java/fr/phoenyx/adventofcode/aoc2020/AdventOfCode14.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2020/AdventOfCode14.java
@@ -3,11 +3,62 @@ package fr.phoenyx.adventofcode.aoc2020;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import fr.phoenyx.utils.Utils;
+
 public class AdventOfCode14 {
+
+    private static class Program {
+        String mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+        Map<Long, Long> memory1 = new HashMap<>();
+        Map<Long, Long> memory2 = new HashMap<>();
+
+        void apply(long address, long value) {
+            apply1(address, value);
+            apply2(address, value);
+        }
+
+        private void apply1(long address, long value) {
+            long maskedValue = 0;
+            for (int i = 0; i < mask.length(); i++) {
+                char c = mask.charAt(mask.length() - 1 - i);
+                if (c == '1') maskedValue += 1L << i;
+                else if (c == 'X') maskedValue += value & (1L << i);
+            }
+            memory1.put(address, maskedValue);
+        }
+
+        private void apply2(long address, long value) {
+            int addressesCount = 1 << Utils.getLetterCount(mask).get('X');
+            for (int xMask = 0; xMask < addressesCount; xMask++) {
+                long maskedAddress = 0;
+                int xSet = 0;
+                for (int i = 0; i < mask.length(); i++) {
+                    char c = mask.charAt(mask.length() - 1 - i);
+                    if (c == '0') maskedAddress += address & (1L << i);
+                    else if (c == '1') maskedAddress += 1L << i;
+                    else {
+                        if ((xMask & (1L << xSet)) > 0) maskedAddress += 1L << i;
+                        xSet++;
+                    }
+                }
+                memory2.put(maskedAddress, value);
+            }
+        }
+
+        long getMemory1Sum() {
+            return memory1.values().stream().reduce(Long::sum).orElseThrow();
+        }
+
+        long getMemory2Sum() {
+            return memory2.values().stream().reduce(Long::sum).orElseThrow();
+        }
+    }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode14.class);
 
@@ -15,11 +66,16 @@ public class AdventOfCode14 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2020/adventofcode14.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            Program program = new Program();
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (currentLine.startsWith("mask")) program.mask = currentLine.split(" = ")[1];
+                else {
+                    String[] split = currentLine.split("] = ");
+                    program.apply(Long.parseLong(split[0].split("mem\\[")[1]), Long.parseLong(split[1]));
+                }
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", program.getMemory1Sum());
+            LOGGER.info("PART 2: {}", program.getMemory2Sum());
         }
     }
 }
