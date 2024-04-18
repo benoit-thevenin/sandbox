5d3a48ceea8925310e77c5c52ffb7cc35c958689
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-04-18 17:41:10 +0200

    Advent of Code 2017-15

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode15.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode15.java
@@ -3,6 +3,8 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +17,34 @@ public class AdventOfCode15 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode15.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            Queue<Long> generators = new LinkedList<>();
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                String[] split = currentLine.split(" ");
+                generators.add(Long.parseLong(split[split.length - 1]));
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", getMatchingCount(new LinkedList<>(generators), true));
+            LOGGER.info("PART 2: {}", getMatchingCount(new LinkedList<>(generators), false));
         }
+    }
+
+    private static int getMatchingCount(Queue<Long> generators, boolean isPart1) {
+        long modulo = 2147483647L;
+        long mask = 0xffff;
+        long aFactor = 16807;
+        long bFactor = 48271;
+        long aCriteria = isPart1 ? 1 : 4;
+        long bCriteria = isPart1 ? 1 : 8;
+        int steps = isPart1 ? 40000000 : 5000000;
+        int count = 0;
+        for (int i = 0; i < steps; i++) {
+            long a = (generators.remove() * aFactor) % modulo;
+            while (a % aCriteria != 0) a = (a * aFactor) % modulo;
+            long b = (generators.remove() * bFactor) % modulo;
+            while (b % bCriteria != 0) b = (b * bFactor) % modulo;
+            if ((a & mask) == (b & mask)) count++;
+            generators.add(a);
+            generators.add(b);
+        }
+        return count;
     }
 }
