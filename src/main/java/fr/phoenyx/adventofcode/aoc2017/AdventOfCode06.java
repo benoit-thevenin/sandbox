ced69132c19cccf77fd65daa0ce09d869c45c98e
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-08-02 17:13:50 +0200

    Advent of Code 2019-15

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode06.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode06.java
@@ -3,15 +3,11 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.AbstractMap;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Objects;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
