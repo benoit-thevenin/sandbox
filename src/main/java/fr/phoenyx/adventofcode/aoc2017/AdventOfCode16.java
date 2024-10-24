267137f6572dfeeed4a67bb5a772b5b29f86a2dc
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-10-24 15:37:21 +0200

    Advent of Code 2017-16

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode16.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode16.java
@@ -3,6 +3,9 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +18,55 @@ public class AdventOfCode16 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2017/adventofcode16.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+            char[] programs = "abcdefghijklmnop".toCharArray();
+            List<String> moves = new ArrayList<>();
+            while ((currentLine = reader.readLine()) != null) moves.addAll(Arrays.asList(currentLine.split(",")));
+            LOGGER.info("PART 1: {}", getDanceResult(programs, moves, 1));
+            LOGGER.info("PART 2: {}", getDanceResult(programs, moves, 1000000000 - 1));
+        }
+    }
+
+    private static String getDanceResult(char[] programs, List<String> moves, int lastIteration) {
+        List<String> configurations = new ArrayList<>();
+        boolean loopFound = false;
+        for (int i = 0; i < lastIteration; i++) {
+            for (String move : moves) performDanceMove(programs, move);
+            if (!loopFound) {
+                String next = new String(programs);
+                int index = configurations.indexOf(next);
+                if (index != -1) {
+                    loopFound = true;
+                    int length = i - index;
+                    i += ((lastIteration - i) / length) * length;
+                } else configurations.add(next);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+        }
+        return new String(programs);
+    }
+
+    private static void performDanceMove(char[] programs, String move) {
+        if (move.charAt(0) == 's') {
+            int size = Integer.parseInt(move.substring(1));
+            char[] save = new char[programs.length - size];
+            System.arraycopy(programs, 0, save, 0, save.length);
+            System.arraycopy(programs, save.length, programs, 0, size);
+            System.arraycopy(save, 0, programs, size, save.length);
+        } else {
+            String[] split = move.substring(1).split("/");
+            if (move.charAt(0) == 'x') {
+                int index1 = Integer.parseInt(split[0]);
+                int index2 = Integer.parseInt(split[1]);
+                char save = programs[index1];
+                programs[index1] = programs[index2];
+                programs[index2] = save;
+            } else {
+                char program1 = split[0].charAt(0);
+                char program2 = split[1].charAt(0);
+                for (int i = 0; i < programs.length; i++) {
+                    if (programs[i] == program1) programs[i] = program2;
+                    else if (programs[i] == program2) programs[i] = program1;
+                }
+            }
         }
     }
 }
