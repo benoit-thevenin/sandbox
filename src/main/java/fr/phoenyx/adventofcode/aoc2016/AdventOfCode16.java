99947a62aacfd2a4d76b8eba67d3dddc338004d3
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-10-24 14:45:05 +0200

    Advent of Code 2016-16

--- src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode16.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2016/AdventOfCode16.java
@@ -16,10 +16,32 @@ public class AdventOfCode16 {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                LOGGER.info("PART 1: {}", getResult(currentLine, 272));
+                LOGGER.info("PART 2: {}", getResult(currentLine, 35651584));
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
         }
+    }
+
+    private static String getResult(String initialState, int length) {
+        String current = initialState;
+        while (current.length() < length) current = dragonCurveFold(current);
+        return getChecksum(current, length);
+    }
+
+    private static String dragonCurveFold(String input) {
+        StringBuilder sb = new StringBuilder(input);
+        sb.append('0');
+        for (int i = input.length() - 1; i >= 0; i--) sb.append(input.charAt(i) == '0' ? '1' : '0');
+        return sb.toString();
+    }
+
+    private static String getChecksum(String input, int length) {
+        String checksum = input.substring(0, length);
+        while (checksum.length() % 2 == 0) {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < checksum.length(); i += 2) sb.append(checksum.charAt(i) == checksum.charAt(i + 1) ? '1' : '0');
+            checksum = sb.toString();
+        }
+        return checksum;
     }
 }
