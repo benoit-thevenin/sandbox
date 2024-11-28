117acfa2b7d925dd4656fed435f6965abcb919d3
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-11-28 14:45:18 +0100

    Advent of Code 2024: init classes

---
+++ src/main/java/fr/phoenyx/adventofcode/aoc2024/AdventOfCode09.java
@@ -0,0 +1,25 @@
+package fr.phoenyx.adventofcode.aoc2024;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class AdventOfCode09 {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode09.class);
+
+    public static void main(String[] args) throws IOException {
+        String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2024/adventofcode09.txt";
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String currentLine;
+            while ((currentLine = reader.readLine()) != null) {
+                // TODO do something
+            }
+            LOGGER.info("PART 1: {}", 0);
+            LOGGER.info("PART 2: {}", 0);
+        }
+    }
+}
