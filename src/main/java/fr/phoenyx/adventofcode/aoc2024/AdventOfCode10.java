f63f3c7b4d4cb0428fe56842d82fe08c68a904cf
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2024-12-10 06:36:33 +0100

    Advent of Code 2024-10

--- src/main/java/fr/phoenyx/adventofcode/aoc2024/AdventOfCode10.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2024/AdventOfCode10.java
@@ -1,11 +1,22 @@
 package fr.phoenyx.adventofcode.aoc2024;
 
+import fr.phoenyx.models.CharGrid;
+import fr.phoenyx.models.coords.Coord2;
+import fr.phoenyx.models.coords.Dir;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class AdventOfCode10 {
 
@@ -15,11 +26,51 @@ public class AdventOfCode10 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2024/adventofcode10.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            List<String> lines = new ArrayList<>();
+            while ((currentLine = reader.readLine()) != null) lines.add(currentLine);
+            CharGrid grid = new CharGrid(lines);
+            Map.Entry<Integer, Integer> result = getResult(grid);
+            LOGGER.info("PART 1: {}", result.getKey());
+            LOGGER.info("PART 2: {}", result.getValue());
         }
+    }
+
+    private static Map.Entry<Integer, Integer> getResult(CharGrid grid) {
+        int score = 0;
+        int rating = 0;
+        for (int i = 0; i < grid.width; i++) {
+            for (int j = 0; j < grid.height; j++) {
+                if (grid.grid[i][j] == '0') {
+                    Map.Entry<Integer, Integer> result = getResult(grid, i, j);
+                    score += result.getKey();
+                    rating += result.getValue();
+                }
+            }
+        }
+        return new AbstractMap.SimpleEntry<>(score, rating);
+    }
+
+
+    private static Map.Entry<Integer, Integer> getResult(CharGrid grid, int i, int j) {
+        int score = 0;
+        int rating = 0;
+        Queue<Coord2> toVisit = new LinkedList<>();
+        toVisit.add(new Coord2(i, j));
+        Set<Coord2> visited = new HashSet<>(toVisit);
+        while (!toVisit.isEmpty()) {
+            Coord2 current = toVisit.remove();
+            int height = grid.grid[current.x][current.y] - '0';
+            for (Dir dir : Dir.FOUR_NEIGHBOURS_VALUES) {
+                Coord2 next = current.move(dir);
+                if (grid.isInGrid(next.x, next.y) && grid.grid[next.x][next.y] - '0' == height + 1) {
+                    boolean isNew = visited.add(next);
+                    if (grid.grid[next.x][next.y] == '9') {
+                        rating++;
+                        if (isNew) score++;
+                    } else toVisit.add(next);
+                }
+            }
+        }
+        return new AbstractMap.SimpleEntry<>(score, rating);
     }
 }
