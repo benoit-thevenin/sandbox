75a2ec4b9394bf40ee25c9c4f100e0d1eeceb5dd
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-12-19 06:51:10 +0100

    Advent of Code 2024-19

--- src/main/java/fr/phoenyx/adventofcode/aoc2024/AdventOfCode19.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2024/AdventOfCode19.java
@@ -6,20 +6,38 @@ import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AdventOfCode19 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode19.class);
+    private static final List<String> TOWELS = new ArrayList<>();
+    private static final Map<String, Long> CACHE = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2024/adventofcode19.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            CACHE.put("", 1L);
             String currentLine;
+            List<String> patterns = new ArrayList<>();
+            boolean isPattern = false;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (currentLine.isEmpty()) isPattern = true;
+                else if (isPattern) patterns.add(currentLine);
+                else TOWELS.addAll(Arrays.stream(currentLine.split(", ")).toList());
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", patterns.stream().map(AdventOfCode19::getCount).filter(c -> c > 0).count());
+            LOGGER.info("PART 2: {}", patterns.stream().map(AdventOfCode19::getCount).reduce(Long::sum).orElseThrow());
         }
+    }
+
+    private static long getCount(String pattern) {
+        if (CACHE.containsKey(pattern)) return CACHE.get(pattern);
+        CACHE.put(pattern, TOWELS.stream().filter(pattern::startsWith).map(t -> getCount(pattern.substring(t.length()))).reduce(Long::sum).orElse(0L));
+        return CACHE.get(pattern);
     }
 }
