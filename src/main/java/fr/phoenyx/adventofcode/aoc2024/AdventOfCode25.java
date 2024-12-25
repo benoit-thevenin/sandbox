47f0fe8e62463036b8787b38bd867c6ecedb1e3d
Benoit THEVENIN <benoit-externe.thevenin@enedis.fr>
2024-12-25 06:45:48 +0100

    Advent of Code 2024-25

--- src/main/java/fr/phoenyx/adventofcode/aoc2024/AdventOfCode25.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2024/AdventOfCode25.java
@@ -1,25 +1,66 @@
 package fr.phoenyx.adventofcode.aoc2024;
 
+import fr.phoenyx.models.CharGrid;
+import fr.phoenyx.models.coords.Coord2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdventOfCode25 {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AdventOfCode25.class);
+    private static int height = -1;
 
     public static void main(String[] args) throws IOException {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2024/adventofcode25.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            List<List<Long>> locks = new ArrayList<>();
+            List<List<Long>> keys = new ArrayList<>();
+            List<String> lines = new ArrayList<>();
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (currentLine.isEmpty()) addLockOrKey(locks, keys, lines);
+                else lines.add(currentLine);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            addLockOrKey(locks, keys, lines);
+            LOGGER.info("SOLUTION: {}", countLockKeyPairFitting(locks, keys));
         }
+    }
+
+    private static void addLockOrKey(List<List<Long>> locks, List<List<Long>> keys, List<String> lines) {
+        CharGrid grid = new CharGrid(lines);
+        if (height == -1) height = grid.height;
+        else if (height != grid.height) throw new IllegalArgumentException("Wrong input: heights don't match");
+        if (grid.grid[0][0] == '#') locks.add(getHeights(grid));
+        else keys.add(getHeights(grid));
+        lines.clear();
+    }
+
+    private static List<Long> getHeights(CharGrid grid) {
+        List<Long> heights = new ArrayList<>();
+        List<Coord2> coords = grid.getCoordinatesMatching(c -> c == '#');
+        for (int i = 0; i < grid.width; i++) {
+            int x = i;
+            heights.add(coords.stream().filter(c -> c.x == x).count());
+        }
+        return heights;
+    }
+
+    private static int countLockKeyPairFitting(List<List<Long>> locks, List<List<Long>> keys) {
+        int count = 0;
+        for (List<Long> lock : locks) {
+            for (List<Long> key : keys) if (!isOverlapping(lock, key)) count++;
+        }
+        return count;
+    }
+
+    private static boolean isOverlapping(List<Long> lock, List<Long> key) {
+        for (int i = 0; i < lock.size(); i++) if (lock.get(i) + key.get(i) > height) return true;
+        return false;
     }
 }
