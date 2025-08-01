4f8d15973a6cc27a8922df15767d26422d24eb55
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2025-08-01 09:31:13 +0200

    Advent of Code 2017-17

--- src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode17.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2017/AdventOfCode17.java
@@ -3,6 +3,8 @@ package fr.phoenyx.adventofcode.aoc2017;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,10 +18,27 @@ public class AdventOfCode17 {
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                int steps = Integer.parseInt(currentLine);
+                Deque<Integer> stack = new ArrayDeque<>();
+                stack.push(0);
+                for (int i = 1; i <= 2017; i++) insertNextNode(stack, steps, i);
+                LOGGER.info("PART 1: {}", stack.stream().toList().get(1));
+                long begin = System.nanoTime();
+                for (int i = 2018; i < 50000000; i++) insertNextNode(stack, steps, i);
+                while (stack.getFirst() != 0) rotateDeque(stack, 1);
+                rotateDeque(stack, 1);
+                LOGGER.info("PART 2: {}, time elapsed: {}ms", stack.getFirst(), (System.nanoTime() - begin) / 1000000);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
         }
+    }
+
+    private static void insertNextNode(Deque<Integer> stack, int steps, int value) {
+        rotateDeque(stack, steps + 1);
+        stack.push(value);
+    }
+
+    private static void rotateDeque(Deque<Integer> deque, int k) {
+        k = k % deque.size();
+        for (int i = 0; i < k; i++) deque.addLast(deque.removeFirst());
     }
 }
