3e512ece7eee1126e4beb78f2c2c8ecca69c5d5a
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2025-12-01 06:38:45 +0100

    Advent of Code 2025-01

--- src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode01.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode01.java
@@ -15,11 +15,21 @@ public class AdventOfCode01 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2025/adventofcode01.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            int dial = 50;
+            int count1 = 0;
+            int count2 = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                boolean isStartZero = dial == 0;
+                int rotation = Integer.parseInt(currentLine.substring(1));
+                dial += currentLine.charAt(0) == 'L' ? -rotation : rotation;
+                count2 += Math.abs(dial) / 100;
+                if (dial <= 0 && !isStartZero) count2++;
+                dial %= 100;
+                if (dial < 0) dial += 100;
+                if (dial == 0) count1++;
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", count1);
+            LOGGER.info("PART 2: {}", count2);
         }
     }
 }
