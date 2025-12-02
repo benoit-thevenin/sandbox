1f54c837d7f16ce574ff6afffa97b35ccc1b5b24
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2025-12-02 06:44:14 +0100

    Advent of Code 2025-02

--- src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode02.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode02.java
@@ -1,5 +1,6 @@
 package fr.phoenyx.adventofcode.aoc2025;
 
+import fr.phoenyx.models.Range;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,11 +16,41 @@ public class AdventOfCode02 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2025/adventofcode02.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            long sum1 = 0;
+            long sum2 = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                for (String s : currentLine.split(",")) {
+                    Range range = Range.buildFromStartAndEndInclusive(s);
+                    for (long i = range.start; i < range.end; i++) {
+                        if (isInvalidId1(i)) sum1 += i;
+                        if (isInvalidId2(i)) sum2 += i;
+                    }
+                }
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", sum1);
+            LOGGER.info("PART 2: {}", sum2);
         }
+    }
+
+    private static boolean isInvalidId1(long id) {
+        String digits = Long.toString(id);
+        return digits.substring(0, digits.length() / 2).equals(digits.substring(digits.length() / 2));
+    }
+
+    private static boolean isInvalidId2(long id) {
+        String digits = Long.toString(id);
+        for (int i = 1; i <= digits.length() / 2; i++) {
+            if (digits.length() % i != 0) continue;
+            String sub = digits.substring(0, i);
+            boolean isInvalid = true;
+            for (int j = 1; j < digits.length() / i; j++) {
+                if (!sub.equals(digits.substring(i * j, i * (j + 1)))) {
+                    isInvalid = false;
+                    break;
+                }
+            }
+            if (isInvalid) return isInvalid;
+        }
+        return false;
     }
 }
