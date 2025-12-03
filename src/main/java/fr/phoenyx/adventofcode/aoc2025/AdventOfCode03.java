f5edcdd17dc9657d40176990f397f09bf7835bf9
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2025-12-03 06:31:17 +0100

    Advent of Code 2025-03

--- src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode03.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode03.java
@@ -15,11 +15,33 @@ public class AdventOfCode03 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2025/adventofcode03.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            long sum1 = 0;
+            long sum2 = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                sum1 += getMaxJoltage(currentLine, 2);
+                sum2 += getMaxJoltage(currentLine, 12);
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            LOGGER.info("PART 1: {}", sum1);
+            LOGGER.info("PART 2: {}", sum2);
         }
+    }
+
+    private static long getMaxJoltage(String bank, int quantity) {
+        int highestIndexOn = -1;
+        StringBuilder sb = new StringBuilder();
+        while (sb.length() < quantity) {
+            int bestIndex = -1;
+            int max = 0;
+            for (int i = bank.length() - quantity + sb.length(); i > highestIndexOn; i--) {
+                int value = bank.charAt(i) - '0';
+                if (value >= max) {
+                    max = value;
+                    bestIndex = i;
+                }
+            }
+            highestIndexOn = bestIndex;
+            sb.append(bank.charAt(bestIndex));
+        }
+        return Long.parseLong(sb.toString());
     }
 }
