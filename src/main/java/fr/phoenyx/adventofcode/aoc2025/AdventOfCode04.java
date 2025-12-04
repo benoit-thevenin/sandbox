db345fd494a6ff2d2f410009ee0390cb6598e538
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2025-12-04 06:23:25 +0100

    Advent of Code 2025-04

--- src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode04.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode04.java
@@ -1,11 +1,16 @@
 package fr.phoenyx.adventofcode.aoc2025;
 
+import fr.phoenyx.models.CharGrid;
+import fr.phoenyx.models.coords.Coord2;
+import fr.phoenyx.models.coords.Dir;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AdventOfCode04 {
 
@@ -15,11 +20,34 @@ public class AdventOfCode04 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2025/adventofcode04.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
-            while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
-            }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            List<String> lines = new ArrayList<>();
+            while ((currentLine = reader.readLine()) != null) lines.add(currentLine);
+            CharGrid grid = new CharGrid(lines);
+            LOGGER.info("PART 1: {}", getAccessibleRolls(grid).size());
+            LOGGER.info("PART 2: {}", countRemoveableRolls(grid));
         }
+    }
+
+    private static List<Coord2> getAccessibleRolls(CharGrid grid) {
+        return grid.getCoordinatesMatching(c -> c == '@').stream().filter(c -> isAccessible(grid, c)).toList();
+    }
+
+    private static int countRemoveableRolls(CharGrid grid) {
+        List<Coord2> accessibleRolls = getAccessibleRolls(grid);
+        int initialCount = grid.getCoordinatesMatching(c -> c == '@').size();
+        while (!accessibleRolls.isEmpty()) {
+            for (Coord2 roll : accessibleRolls) grid.set(roll, '.');
+            accessibleRolls = getAccessibleRolls(grid);
+        }
+        return initialCount - grid.getCoordinatesMatching(c -> c == '@').size();
+    }
+
+    private static boolean isAccessible(CharGrid grid, Coord2 coord) {
+        int count = 0;
+        for (Dir dir : Dir.values()) {
+            Coord2 neighbour = coord.move(dir);
+            if (grid.isInGrid(neighbour) && grid.get(neighbour) == '@') count++;
+        }
+        return count < 4;
     }
 }
