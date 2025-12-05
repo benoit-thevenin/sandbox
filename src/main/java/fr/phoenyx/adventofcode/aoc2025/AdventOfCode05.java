e22fa6c964c2a503f043ae6b0674e1059e069eea
Benoit THEVENIN <benoit.thevenin@telecomnancy.net>
2025-12-05 06:22:09 +0100

    Advent of Code 2025-05

--- src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode05.java
+++ src/main/java/fr/phoenyx/adventofcode/aoc2025/AdventOfCode05.java
@@ -1,11 +1,13 @@
 package fr.phoenyx.adventofcode.aoc2025;
 
+import fr.phoenyx.models.Range;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class AdventOfCode05 {
 
@@ -15,11 +17,32 @@ public class AdventOfCode05 {
         String filePath = "src/main/resources/fr/phoenyx/adventofcode/aoc2025/adventofcode05.txt";
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String currentLine;
+            Set<Range> ranges = new HashSet<>();
+            boolean isRange = true;
+            int count = 0;
             while ((currentLine = reader.readLine()) != null) {
-                // TODO do something
+                if (currentLine.isEmpty()) isRange = false;
+                else if (isRange) ranges.add(Range.buildFromStartAndEndInclusive(currentLine));
+                else {
+                    long id = Long.parseLong(currentLine);
+                    if (ranges.stream().anyMatch(r -> r.isInRange(id))) count++;
+                }
             }
-            LOGGER.info("PART 1: {}", 0);
-            LOGGER.info("PART 2: {}", 0);
+            List<Range> nonOverlappingRanges = new ArrayList<>();
+            while (!ranges.isEmpty()) {
+                Range range = ranges.iterator().next();
+                ranges.remove(range);
+                while (true) {
+                    Range finalRange = range;
+                    Optional<Range> overlappingRange = ranges.stream().filter(r -> finalRange.intersection(r).isPresent()).findFirst();
+                    if (overlappingRange.isEmpty()) break;
+                    range = range.union(overlappingRange.get());
+                    ranges.remove(overlappingRange.get());
+                }
+                nonOverlappingRanges.add(range);
+            }
+            LOGGER.info("PART 1: {}", count);
+            LOGGER.info("PART 2: {}", nonOverlappingRanges.stream().map(r -> r.length).reduce(0L, Long::sum));
         }
     }
 }
